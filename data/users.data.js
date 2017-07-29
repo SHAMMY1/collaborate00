0da1713c2a534f08e131891ad99ba116ce9f3de4
SHAMMY <Shammy.bg1@gmail.com>
2017-07-29 18:40:40 +0300

    add data

---
+++ data/users.data.js
@@ -0,0 +1,26 @@
+const BaseData = require('./base');
+const { User } = require('../models');
+
+class UsersData extends BaseData {
+    constructor(db) {
+        super(db, User, User);
+    }
+
+    checkPassword(username, password) {
+        return this.collection.findOne({
+            username,
+        })
+            .then((user) => {
+                if (!user) {
+                    throw new Error('Invalid user');
+                }
+                if (user.password !== password) {
+                    throw new Error('Invalid password!');
+                }
+
+                return true;
+            });
+    }
+}
+
+module.exports = UsersData;
