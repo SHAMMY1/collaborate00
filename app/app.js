994b72db2342c3128d3b0614c8be9b27f1a92618
SHAMMY <Shammy.bg1@gmail.com>
2017-07-30 20:59:49 +0300

    add passport, cookie and body parser, express-session and passport local strategy

--- app/app.js
+++ app/app.js
@@ -2,11 +2,25 @@ const init = (data) => {
     const express = require('express');
     const app = express();
     const controllers = require('./controllers').init(data);
+
+    const bodyParser = require('body-parser');
+    const cookieParser = require('cookie-parser');
+    const session = require('express-session');
+    const passport = require('passport');
+    const LocalStrategy = require('passport-local');
+
+    // setup view engine
     app.set('view engine', 'pug');
     app.set('views', './views');
 
+    // setup static
     app.use('/', express.static('./public'));
 
+    // setup session
+
+    // setup passport
+
+    // setup routes
     require('./routes').atachTo(app, controllers);
 
     return app;
